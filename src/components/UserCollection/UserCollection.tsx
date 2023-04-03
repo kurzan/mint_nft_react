c1532c77646974e2d6ae8737ff7ceb7f50ce2c0f
Kurzov <kurzanbl4@gmail.com>
2023-04-03 17:25:00 +0300

    fetch user NFTs

--- src/components/UserCollection/UserCollection.tsx
+++ src/components/UserCollection/UserCollection.tsx
@@ -1,16 +1,24 @@
-import { useState } from "react";
+/* eslint-disable react/jsx-no-comment-textnodes */
+import { useEffect, useState, useCallback } from "react";
 import Box from "../Box/Box";
 import ImagePreview from "../ImagePreview/ImagePreview";
 import styles from "./UserCollection.module.css";
 import { contract } from "@/services/web3config";
-import { useContractRead } from 'wagmi';
+import { useContractRead, useContract, useAccount, useProvider  } from 'wagmi';
 
 
 const UserCollection = () => {
+  const { address } = useAccount();
+  const provider = useProvider();
 
-  const [isCollection, setIsCollection] = useState(false);
+  const contractNft = useContract({
+    address: contract.address,
+    abi: contract.abi,
+    signerOrProvider: provider,
+  });
 
   const [minted, setMinted] = useState(0);
+  const [userNfts, setUserNfts] = useState<Array<number>>([]);
 
   const { data: mintedCount, isError, isLoading: isLoadingMintetCount } = useContractRead({
     address: contract.address,
@@ -22,31 +30,45 @@ const UserCollection = () => {
     }
   });
 
-  let tokens;
+  const fetchNftList = useCallback(async () => {
 
-  for (let index = 0; index < minted; index++) {
-    
-  }
+    let tokens = [];
 
+    for (let index = 0; index < minted; index++) {
+      const tokenOwner = await contractNft?.ownerOf(index);
+      if (tokenOwner.toLowerCase() === address?.toLowerCase()) {
+        tokens.push(index);
+      }
+    };
+
+    setUserNfts(tokens);
+
+  },[minted, contractNft, address]);
+
+  useEffect(() => {
+    fetchNftList();
+  }, [fetchNftList])
 
 
   return(
     <Box>
       <div className={styles.container}>
-        <p className={styles.title}>Your collection</p>
-        {!isCollection ? (
+        <p className={styles.title}>Your NFT`s</p>
+        {!userNfts.length ? (
           <p>You dont have NFT. Please mint them</p>
         )
           :
          (
           <div className={styles.list}>
-            <ImagePreview src="/nft/5.jpg"/>
-            <ImagePreview src="/nft/5.jpg"/>
-            <ImagePreview src="/nft/5.jpg"/>
+            {userNfts.map((nft) => (
+              <div key={nft} className={styles.item}>
+                <ImagePreview src={`/nft/${nft}.jpg`}/>
+                <p>Smoke Cat #{nft}</p><a target={blank_} href={`https://testnets.opensea.io/collection/smoke-cat-${nft}`}>View on OpenSea</a>
+              </div>
+            ))}
           </div>
          )
         }
-        
       </div>
     </Box>
   )
